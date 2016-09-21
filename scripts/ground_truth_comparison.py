d8ecc18fd5931125717d2e339eb33e3db98ddde3
Nico Valigi <nicolov@users.noreply.github.com>
2016-09-21 16:36:30 +0200

    Improve plotting

--- scripts/ground_truth_comparison.py
+++ scripts/ground_truth_comparison.py
@@ -16,6 +16,9 @@ import numpy as np
 GROUND_TRUTH_PATH = os.path.expanduser(
     '~/bags/IJRR_2008_Dataset/Data/NewCollege/masks/NewCollegeGroundTruth.mat')
 
+WORK_FOLDER = os.path.expanduser(
+    '~/dev/simple_slam_loop_closure/out/')
+
 if __name__ == "__main__":
 
     default_heatmap_kwargs = dict(
@@ -31,16 +34,21 @@ if __name__ == "__main__":
     sns.heatmap(gt_data[::2, ::2],
         ax=ax1,
         **default_heatmap_kwargs)
+    ax1.set_title('Ground truth')
 
     # Plot the BoW results
-    bow_data = np.loadtxt(
-        os.path.expanduser(
-            '~/dev/simple_slam_loop_closure/confusion_matrix.txt'))
+    bow_data = np.loadtxt(os.path.join(
+        WORK_FOLDER, 'confusion_matrix.txt'))
     # Take the lower triangle only
     bow_data = np.tril(bow_data, 0)
     sns.heatmap(bow_data,
         ax=ax2,
         vmax=0.2,
         **default_heatmap_kwargs)
+    ax2.set_title('SURF + BoW')
 
-    plt.show()
+    # plt.show()
+    plt.tight_layout()
+    plt.savefig(os.path.join(
+        WORK_FOLDER, 'confusion_matrix.png'),
+        bbox_inches='tight')
